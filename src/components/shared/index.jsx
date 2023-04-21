bc97ac40155a8001b3c0ec9f3787d9f3f46df5b1
Minoo <minoo.hadi2904@gmail.com>
2023-04-21 17:14:40 +0430

    feat: create layers component

--- src/components/shared/index.jsx
+++ src/components/shared/index.jsx
@@ -2,4 +2,4 @@ export { default as Input } from "./input/index";
 export { default as Button } from "./button/index";
 export { default as Header } from "./header/index";
 export { default as ProjectCart } from "./projectCart/index";
-
+export { default as Map } from "./map/index";
---
+++ src/components/shared/layers/TileLayer.jsx
@@ -0,0 +1,23 @@
+import React, { useContext, useEffect } from "react";
+import MapContext from "../map/MapContext";
+import OLTileLayer from "ol/layer/Tile";
+
+function TileLayer({ source, zIndex = 0 }) {
+  const { map } = useContext(MapContext);
+
+  useEffect(() => {
+    if (!map) return;
+
+    let tileLayer = new OLTileLayer({ source, zIndex });
+    map.addLayer(tileLayer);
+    tileLayer.setZIndex(zIndex);
+    return () => {
+      if (map) {
+        map.removeLayer(tileLayer);
+      }
+    };
+  }, [map]);
+  return null;
+}
+
+export default TileLayer;
