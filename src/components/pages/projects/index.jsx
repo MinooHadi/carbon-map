ca5264ac92127b81a1db1e52410274290d08de29
Minoo <minoo.hadi2904@gmail.com>
2023-04-21 13:11:16 +0430

    feat: create project cart component

--- src/components/pages/projects/index.jsx
+++ src/components/pages/projects/index.jsx
@@ -6,14 +6,18 @@ function Projects() {
   return (
     <>
       <Header />
-      <div className="bg-gray-50 h-[91vh] ">
-        <div className="w-[80%] m-auto py-5 flex justify-between">
+      <div className="bg-gray-50">
+        <div className="w-[80%] m-auto py-6 flex justify-between items-center mb-3">
           <h1 className="text-xl font-semibold">
             Projects <span className="text-gray-400">5</span>
           </h1>
           <div className="relative flex items-center">
             <SearchAlt2 className="absolute left-2" size="1.2rem" />
-            <Input className="bg-gray-200 py-1 pl-8 w-[90%] rounded-md" type="Search" placeholder="Search" />
+            <Input
+              className="bg-gray-200 py-1 pl-8 rounded-md focus:w-[300px] "
+              type="Search"
+              placeholder="Search"
+            />
           </div>
           <Button
             className="flex items-center flex-row-reverse bg-emerald-400 text-white font-semibold py-1 px-3 rounded-md gap-1"
@@ -21,7 +25,13 @@ function Projects() {
             icon={<FillPlusCircle size="1.2rem" />}
           />
         </div>
-        <ProjectCart />
+        <div className="w-[80%] m-auto flex flex-wrap gap-6">
+          <ProjectCart />
+          <ProjectCart />
+          <ProjectCart />
+          <ProjectCart />
+          <ProjectCart />
+        </div>
       </div>
     </>
   );
--- src/components/shared/projectCart/index.jsx
+++ src/components/shared/projectCart/index.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import image from "./../../../assets/image/JungleValley-22x32-boreal-preview.webp";
+import flag from "./../../../assets/image/download.png";
 
 function ProjectCart() {
-  return <h1>cart</h1>;
+  return (
+    <div className="w-[32%] bg-white rounded-xl shadow-lg">
+      <img src={image} className="w-full h-[60%] object-scale-cover rounded-t-xl" />
+      <div className="py-4 px-6">
+        <h1 className="text-xl font-bold pb-2">Title</h1>
+        <p className="text-sm font-semibold text-gray-700 pb-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quis
+          explicabo esse praesentium quam perferendis inventore voluptatum.
+        </p>
+        <div className="flex justify-between text-xs font-bold text-gray-400">
+          <p>07.06.2021</p>
+          <div className="flex items-center gap-2">
+            <img src={flag} className="w-6 h-fit" />
+            <p>Germany</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectCart;
