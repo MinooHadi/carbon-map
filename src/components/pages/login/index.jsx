06f1ff28ed059c1583b5531e9b287e7f40a81880
Minoo <minoo.hadi2904@gmail.com>
2023-04-22 22:27:15 +0430

    feat: set background image

--- src/components/pages/login/index.jsx
+++ src/components/pages/login/index.jsx
@@ -3,17 +3,24 @@ import { Button, Input } from "../../shared";
 import { Link, useNavigate } from "react-router-dom";
 import { ArrowRight, Lock, UserAlt } from "../../icons";
 import logo from "./../../../assets/logo/CCLogo-Black.png";
+import jungle from "./../../../assets/image/PL3hpY.jpg"
 
 function Login() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <div className="bg-emerald-50 h-[100vh] pt-[10vh]">
-      <div className="bg-gray-50 h-[80vh] w-[90%] m-auto flex justify-between px-64 pt-20 gap-8 shadow-md">
-        <div className="w-[60%] py-3 ">
+      <div
+        className="bg-gray-50 h-[80vh] w-[90%] m-auto flex justify-between px-64 pt-20 gap-8 shadow-md relative"
+      >
+        <img src={jungle} className="absolute top-0 left-0 h-[100%] w-[100%] opacity-20 object-cover" />
+        <div className="w-[60%] py-3 z-10 ">
           <div className="flex items-center">
             <img src={logo} className="w-12" />
-            <h1 className="text-3xl font-bold"> <span className="text-emerald-500">Carbon</span> Map</h1>
+            <h1 className="text-3xl font-bold">
+              {" "}
+              <span className="text-emerald-500">Carbon</span> Map
+            </h1>
           </div>
           <p className="text-gray-500 font-semibold text-2xl pt-6">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates
@@ -21,8 +28,8 @@ function Login() {
             commodi eos!
           </p>
         </div>
-        <hr className="w-[2px] h-64 bg-gray-200" />
-        <div className="flex flex-col gap-3 w-[40%] py-3 ">
+        <hr className="w-[2px] h-64 bg-gray-600 z-10" />
+        <div className="flex flex-col gap-3 w-[40%] py-3 z-10 ">
           <div className="relative flex items-center w-[100%] ">
             <UserAlt className="absolute left-2" />
             <Input
@@ -50,9 +57,11 @@ function Login() {
               onClick={() => navigate("/projects")}
             />
           </div>
-          <hr className="h-[1.5px] w-[100%] bg-gray-200 my-3" />
+          <hr className="h-[3px] w-[100%] bg-gray-600 my-3" />
           <div className="flex items-center w-[100%] justify-center gap-2">
-            <p className="text-gray-500 text-xs font-semibold">No account yet?</p>
+            <p className="text-gray-500 text-xs font-semibold">
+              No account yet?
+            </p>
             <Link to="#" className="text-emerald-500 text-sm font-semibold">
               Sign Up
             </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // backgroundImage: {
+      //   "jungle": "url('./src/assets/image/PL3hpY.jpg')"
+      // }
+    },
   },
   plugins: [],
 };
