64564c83f921ab87f2b03a6d387da259f601637c
Minoo <minoo.hadi2904@gmail.com>
2023-04-29 00:35:23 +0430

    refactor: refactor input props

--- src/components/shared/input/index.jsx
+++ src/components/shared/input/index.jsx
@@ -3,11 +3,14 @@ import React from "react";
 function Input(props) {
   return (
     <div className="flex flex-col w-[100%]">
-      <label className="text-sm font-medium text-gray-500 mb-1"> {props.lable} </label>
+      <label className="text-sm font-medium text-gray-500 mb-1">
+        {props.lable}
+      </label>
       <input
         className={props.className}
         type={props.type}
         placeholder={props.placeholder}
+        name={props.name}
       />
     </div>
   );
