619e3a49dcc191f207d3f4cc482f9bde74779db9
Minoo <minoo.hadi2904@gmail.com>
2023-05-02 19:39:57 +0430

    feat: add header for home page

--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -8,13 +8,28 @@ import {
 } from "../../shared";
 import { fromLonLat } from "ol/proj";
 import xyz from "../../../Source/xyz";
+import logo from "./../../../assets/logo/CCLogo-Black.png";
+import { UserAlt } from "../../icons";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate()
   const [center, setCenter] = useState([-100.9065, 57.9884]);
   const [zoom, setZoom] = useState(5);
 
   return (
     <div>
+      <div className="w-[100%] absolute top-0 z-10 flex justify-between h-[10vh] items-center px-5 bg-white opacity-70">
+        <div className="flex items-center">
+          <img src={logo} className="w-12" />
+          <h1 className="text-2xl font-bold">
+            <span className="text-emerald-500">C-Carbon</span> Map
+          </h1>
+        </div>
+        <div className="px-2 py-2 bg-emerald-400 rounded-lg">
+          <UserAlt size="1.5rem" color="white" onClick={() => navigate("/login")} />
+        </div>
+      </div>
       <Map
         center={fromLonLat(center)}
         zoom={zoom}
