c4d94bfd8b8107bb4c66cbb84bd2754764d2064f
Minoo <minoo.hadi2904@gmail.com>
2023-05-04 14:39:17 +0430

    feat: check autjentication in projects page

--- src/components/pages/projects/index.jsx
+++ src/components/pages/projects/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button, Header, Input, ProjectCart } from "../../shared";
+import { Auth, Button, Header, Input, ProjectCart } from "../../shared";
 import { FillPlusCircle, SearchAlt2 } from "../../icons";
 import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../../../api";
@@ -15,49 +15,51 @@ function Projects() {
   }, []);
 
   return (
-    <div>
-      <Header />
-      <div className="bg-gray-50">
-        <div className="w-[80%] m-auto py-6 flex justify-between items-center mb-3">
-          <h1 className="text-xl font-semibold">
-            Projects{" "}
-            <span className="text-gray-400"> {projectsList.length} </span>
-          </h1>
-          <div className="relative flex items-center">
-            <SearchAlt2 className="absolute left-2" size="1.2rem" />
-            <Input
-              className="bg-gray-200 py-1 pl-8 rounded-md focus:w-[300px] "
-              type="Search"
-              placeholder="Search"
+    <Auth>
+      <div>
+        <Header />
+        <div className="bg-gray-50">
+          <div className="w-[80%] m-auto py-6 flex justify-between items-center mb-3">
+            <h1 className="text-xl font-semibold">
+              Projects{" "}
+              <span className="text-gray-400"> {projectsList.length} </span>
+            </h1>
+            <div className="relative flex items-center">
+              <SearchAlt2 className="absolute left-2" size="1.2rem" />
+              <Input
+                className="bg-gray-200 py-1 pl-8 rounded-md focus:w-[300px] "
+                type="Search"
+                placeholder="Search"
+              />
+            </div>
+            <Button
+              className="flex items-center flex-row-reverse bg-emerald-400 text-white font-semibold py-1 px-3 rounded-md gap-1"
+              title="New Project"
+              icon={<FillPlusCircle size="1.2rem" />}
+              onClick={() => navigate("/create")}
             />
           </div>
-          <Button
-            className="flex items-center flex-row-reverse bg-emerald-400 text-white font-semibold py-1 px-3 rounded-md gap-1"
-            title="New Project"
-            icon={<FillPlusCircle size="1.2rem" />}
-            onClick={() => navigate("/create")}
-          />
-        </div>
-        <div className="w-[80%] m-auto flex flex-wrap gap-6">
-          {projectsList.map((item) => {
-            return (
-              <ProjectCart
-                id={item.id}
-                thumbnail={item.thumbnail}
-                name={item.name}
-                description={item.description}
-                createdAt={
-                  new Date(item.created_at).toISOString().split("T")[0]
-                }
-                onClick={(e) =>
-                  navigate(`/detail/${e.currentTarget.dataset.id}`)
-                }
-              />
-            );
-          })}
+          <div className="w-[80%] m-auto flex flex-wrap gap-6">
+            {projectsList.map((item) => {
+              return (
+                <ProjectCart
+                  id={item.id}
+                  thumbnail={item.thumbnail}
+                  name={item.name}
+                  description={item.description}
+                  createdAt={
+                    new Date(item.created_at).toISOString().split("T")[0]
+                  }
+                  onClick={(e) =>
+                    navigate(`/detail/${e.currentTarget.dataset.id}`)
+                  }
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
+    </Auth>
   );
 }
 
