d5e9f47e39b8001e9316dfdfdeffe978be95730d
0xNima <neemaiox@gmail.com>
2023-05-15 15:57:52 +0330

    transfer most logics from create new page to report page

--- src/components/pages/createNewProject/index.jsx
+++ src/components/pages/createNewProject/index.jsx
@@ -1,204 +1,162 @@
-import React, { useContext, useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   Auth,
   Button,
-  Contorols,
-  DragAndDrop,
-  DrawItem,
-  FullScreenControl,
   Header,
   Input,
-  Layers,
-  Map,
-  TileLayer,
-  VectorLayer,
 } from "../../shared";
-import { fromLonLat, transform } from "ol/proj";
-import xyz from "../../../Source/xyz";
-import vector from "../../../Source/vector";
 import {
-  MousePointer,
-  OutlineLocationMarker,
-  OutlinePolyline,
-  ShapePolygon,
   Upload,
 } from "../../icons";
-import { GeoJSON, KML } from "ol/format";
-import MapContext from "../../shared/map/MapContext";
-import { Draw } from "ol/interaction";
-import { Vector as VectorSource } from "ol/source";
-import { getCenter } from "ol/extent";
 import { BASE_URL } from "../../../api";
 import { useNavigate } from "react-router-dom";
-import { shapefileTogeojson } from "../../../utils";
 
 
 function CreateNewProject() {
   const navigate = useNavigate();
-  const [center, setCenter] = useState([-103.9065, 56.9884]);
-  const [zoom, setZoom] = useState(5);
-  const [geoFileObject, setGeoFileObject] = useState();
-  const [showToolbar, setShowToolbar] = useState(false);
-  const { map } = useContext(MapContext);
+  // const [center, setCenter] = useState([-103.9065, 56.9884]);
+  // const [zoom, setZoom] = useState(5);
+  // const [geoFileObject, setGeoFileObject] = useState();
+  // const [showToolbar, setShowToolbar] = useState(false);
+  // const { map } = useContext(MapContext);
   const [country, setCountry] = useState([]);
   const [selectedCountry, setSelectedCountry] = useState(undefined);
   const [addresses, setAddresses] = useState({});
 
-  const drawSource = useRef(vector({ wrapX: false }));
-  const drawObj = useRef();
-  const vectorSource = useRef(new VectorSource());
+  // const drawSource = useRef(vector({ wrapX: false }));
+  // const drawObj = useRef();
+  // const vectorSource = useRef(new VectorSource());
 
-  const [labels, setLabels] = useState({});
-  const [state, setState] = useState({});
-  const [dragedFile, setDragedFile] = useState();
+  // const [dragedFile, setDragedFile] = useState();
   const formRef = useRef();
 
   const countrySelectRef = useRef();
   const provinceSelectRef = useRef();
 
-  useEffect(() => {
-    return () => {
-      if (map) {
-        map.removeInteraction(drawObj.current);
-        map.removeInteraction(drawSource.current);
-        map.removeInteraction(vectorSource.current);
-      }
-    };
-  }, []);
+  // useEffect(() => {
+  //   return () => {
+  //     if (map) {
+  //       map.removeInteraction(drawObj.current);
+  //       map.removeInteraction(drawSource.current);
+  //       map.removeInteraction(vectorSource.current);
+  //     }
+  //   };
+  // }, []);
 
-  const xyzSource = useMemo(() => {
-    return xyz({
-      url: "http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}",
-      maxZoom: 20,
-    });
-  }, []);
+  // const xyzSource = useMemo(() => {
+  //   return xyz({
+  //     url: "http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}",
+  //     maxZoom: 20,
+  //   });
+  // }, []);
 
-  useEffect(() => {
-    if (!geoFileObject) return;
+  // useEffect(() => {
+  //   if (!geoFileObject) return;
 
-    vectorSource.current.addFeatures(
-      new geoFileObject.format({
-        featureProjection: "EPSG:3857",
-        dataProjection: 'EPSG:4326'
-      }).readFeatures(geoFileObject.data)
-    );
-    let polygonCenter = getCenter(vectorSource.current.getExtent());
-    setCenter(transform(polygonCenter, "EPSG:3857", "EPSG:4326"));
-    setZoom(4);
-  }, [geoFileObject]);
+  //   vectorSource.current.addFeatures(
+  //     new geoFileObject.format({
+  //       featureProjection: "EPSG:3857",
+  //       dataProjection: 'EPSG:4326'
+  //     }).readFeatures(geoFileObject.data)
+  //   );
+  //   let polygonCenter = getCenter(vectorSource.current.getExtent());
+  //   setCenter(transform(polygonCenter, "EPSG:3857", "EPSG:4326"));
+  //   setZoom(4);
+  // }, [geoFileObject]);
 
 
-  async function processFile(file) {
-    const fileFormat = file.name.split(".").pop();
+  // async function processFile(file) {
+  //   const fileFormat = file.name.split(".").pop();
 
-    let data;
-    let format;
+  //   let data;
+  //   let format;
 
-    switch (fileFormat) {
-      case "geojson":
-        data = JSON.parse(await file.text());
-        format = GeoJSON;    
-        break;
-      case "kml":
-        data = await file.text();
-        format = KML;
-        break;
-      case "zip":
-      case "shp":
-        try {
-          data = await shapefileTogeojson(file);
-          format = GeoJSON;
-          break;
-        } catch(err) {
-          alert(err.message);
-          return
-        }
-      default:
-        alert('Unsupported geo data file!');
-        return
-    }
+  //   switch (fileFormat) {
+  //     case "geojson":
+  //       data = JSON.parse(await file.text());
+  //       format = GeoJSON;    
+  //       break;
+  //     case "kml":
+  //       data = await file.text();
+  //       format = KML;
+  //       break;
+  //     case "zip":
+  //     case "shp":
+  //       try {
+  //         data = await shapefileTogeojson(file);
+  //         format = GeoJSON;
+  //         break;
+  //       } catch(err) {
+  //         alert(err.message);
+  //         return
+  //       }
+  //     default:
+  //       alert('Unsupported geo data file!');
+  //       return
+  //   }
     
-    vectorSource.current.clear();
-    countrySelectRef.current.selectedIndex = 0;
-    provinceSelectRef.current.selectedIndex = 0;
+  //   vectorSource.current.clear();
+  //   countrySelectRef.current.selectedIndex = 0;
+  //   provinceSelectRef.current.selectedIndex = 0;
 
-    setSelectedCountry();
-    setGeoFileObject({ data, format});
-  }
+  //   setSelectedCountry();
+  //   setGeoFileObject({ data, format});
+  // }
 
-  async function getFile(e) {
-    await processFile(e.target.files[0]);
-  }
+  // async function getFile(e) {
+  //   await processFile(e.target.files[0]);
+  // }
 
   async function addressSelected(e) {
     try {
-      const res = await fetch(`${BASE_URL}/${e.target.value}`, {
-        method: "GET",
-        credentials: 'include'
-      });
-      if (res.status === 200) {
-        vectorSource.current.clear();
-        setGeoFileObject({ data: await res.json(), format: GeoJSON });
-      }
+      
     } catch (err) {
       alert(err);
     }
   }
 
-  function draw(e) {
-    map.removeInteraction(drawObj.current);
-    drawObj.current = new Draw({
-      source: drawSource.current,
-      type: e.currentTarget.dataset.type,
-    });
-    map.addInteraction(drawObj.current);
-    drawObj.current.on("drawend", (e) => {
-      const feature = e.feature;
-      const id = Date.now();
-      feature.setId(id);
-      let label = prompt("enter label");
-      setState({ [id]: label });
-    });
-  }
+  // function draw(e) {
+  //   map.removeInteraction(drawObj.current);
+  //   drawObj.current = new Draw({
+  //     source: drawSource.current,
+  //     type: e.currentTarget.dataset.type,
+  //   });
+  //   map.addInteraction(drawObj.current);
+  //   drawObj.current.on("drawend", (e) => {
+  //     const feature = e.feature;
+  //     const id = Date.now();
+  //     feature.setId(id);
+  //     let label = prompt("enter label");
+  //     setState({ [id]: label });
+  //   });
+  // }
 
-  function deleteItem(e) {
-    const featureId = +e.currentTarget.dataset.id;
-    const feature = drawSource.current.getFeatureById(featureId);
-    drawSource.current.removeFeature(feature);
-    delete labels[featureId];
-    setLabels({ ...labels });
-  }
-
-  useEffect(() => {
-    setLabels({ ...labels, ...state });
-  }, [state]);
-
-  useEffect(() => {
-    if(dragedFile) processFile(dragedFile);
-  }, [dragedFile]);
+  // useEffect(() => {
+  //   if(dragedFile) processFile(dragedFile);
+  // }, [dragedFile]);
 
   function createProject() {
     const data = new FormData(formRef.current);
 
-    if (!data.get("geo_data_file").name) {
-      if (dragedFile && geoFileObject) {
-        data.set("geo_data_file", dragedFile);
-      } else {
-        const drawData = drawSource.current.getFeatures();
+    // if (!data.get("geo_data_file").name) {
+    //   if (dragedFile && geoFileObject) {
+    //     data.set("geo_data_file", dragedFile);
+    //   } else {
+    //     const drawData = drawSource.current.getFeatures();
 
-        if (drawData) {
-          const format = new GeoJSON();
-          const geoJsonStr = format.writeFeatures(drawData, {
-            dataProjection: "EPSG:4326",
-            featureProjection: "EPSG:3857",
-          });
-          const file = new File([geoJsonStr], `draw-${Date.now()}.geojson`, {
-            type: "text/plain",
-          });
-          data.set("geo_data_file", file);
-        }
-      }
-    }
+    //     if (drawData) {
+    //       const format = new GeoJSON();
+    //       const geoJsonStr = format.writeFeatures(drawData, {
+    //         dataProjection: "EPSG:4326",
+    //         featureProjection: "EPSG:3857",
+    //       });
+    //       const file = new File([geoJsonStr], `draw-${Date.now()}.geojson`, {
+    //         type: "text/plain",
+    //       });
+    //       data.set("geo_data_file", file);
+    //     }
+    //   }
+    // }
     fetch(`${BASE_URL}/project`, {
       method: "POST",
       body: data,
@@ -207,8 +165,6 @@ function CreateNewProject() {
       .then((res) => {
         if (res.status === 201) {
           formRef.current.reset();
-          setGeoFileObject();
-          vectorSource.current.clear();
           return res.json();
         }
         return res.json().then((err) => {
@@ -216,7 +172,7 @@ function CreateNewProject() {
         });
       })
       .then((data) => {
-        navigate(`/query?pid=${data.id}`);
+        navigate(`/report?pid=${data.id}`);
       })
       .catch((err) => alert(err.message));
   }
@@ -231,7 +187,7 @@ function CreateNewProject() {
 
   useEffect(() => {
     if (selectedCountry) {
-      fetch(`${BASE_URL}/api/geodata/`, {
+      fetch(``, {
         method: "POST",
         body: JSON.stringify({
           country: selectedCountry,
@@ -272,8 +228,8 @@ function CreateNewProject() {
               />
             </div>
           </div>
-          <div className="w-[80%] h-[75vh] m-auto flex flex-wrap gap-[2%] pb-12 ">
-            <div className="w-[28%] bg-white shadow-lg rounded-lg">
+          <div className="w-[80%] m-auto flex flex-wrap justify-center items-center">
+            <div className="w-[40%] bg-white shadow-lg rounded-lg">
               <form id="form" ref={formRef}>
                 <div className="bg-gray-200 h-52 rounded-t-lg flex flex-col justify-center items-center">
                   <input type="file" id="imgUpload" hidden name="thumbnail" />
@@ -282,7 +238,7 @@ function CreateNewProject() {
                     htmlFor="imgUpload"
                     className="text-white font-semibold"
                   >
-                    Upload image
+                    Upload preview image
                   </label>
                 </div>
                 <div className="p-[2%]">
@@ -293,6 +249,7 @@ function CreateNewProject() {
                     className="border-2 border-gray-300 px-1 rounded-md mb-4"
                     placeholder="Project name"
                   />
+                
                   <label className="text-sm font-medium text-gray-500">
                     Description
                   </label>
@@ -301,142 +258,44 @@ function CreateNewProject() {
                     className="border-2 border-gray-300 rounded-md w-[100%] px-1 mt-1 "
                     placeholder="Short description"
                   />
-                </div>
-              </form>
 
-              {/* <div className="mt-3">
-                <h1 className="font-semibold text-lg mb-2">Users</h1>
-                <div className="flex flex-col">
                   <label className="text-sm font-medium text-gray-500 mb-1">
-                    Project users
+                    Choose country
                   </label>
-                  <select className="border-2 border-gray-300 px-1 rounded-md mb-4">
-                    <option value="" disabled selected>
-                      Select user
-                    </option>
+                  <br/>
+                  <select
+                    className="border-2 border-gray-300 w-[50%] px-1 rounded-md mb-2"
+                    form="form"
+                    name="country"
+                    onChange={(e) => setSelectedCountry(e.target.value)}
+                    ref={countrySelectRef}
+                  >
+                    <option value=""></option>
+                    {country.map((item, i) => (
+                      <option value={item.code} key={i}> {item.name} </option>
+                    ))}
+                  </select>
+                  
+                  <br/>
+
+                  <label className="text-sm font-medium text-gray-500 mb-1">
+                    Choose the most accurate address
+                  </label>
+                  <br/>
+                  <select
+                    className="border-2 border-gray-300 w-[50%] px-1 rounded-md mb-2"
+                    form="form"
+                    name="province"
+                    onChange={addressSelected}
+                    ref={provinceSelectRef}
+                  >
+                    <option value=""></option>
+                    {Object.entries(addresses).map((item, i) => (
+                      <option value={item[1]} key={i}>{item[0]}</option>
+                    ))}
                   </select>
                 </div>
-              </div> */}
-            </div>
-            <div className="w-[70%] bg-white p-5 shadow-lg rounded-lg">
-              <h1 className="text-lg font-semibold">Province</h1>
-              <div className="flex flex-col mt-1">
-                <label className="text-sm font-medium text-gray-500 mb-1">
-                  Choose country
-                </label>
-                <select
-                  className="border-2 border-gray-300 px-1 rounded-md mb-2"
-                  form="form"
-                  name="country"
-                  onChange={(e) => setSelectedCountry(e.target.value)}
-                  ref={countrySelectRef}
-                >
-                  <option value=""></option>
-                  {country.map((item, i) => (
-                    <option value={item.code} key={i}> {item.name} </option>
-                  ))}
-                </select>
-                <label className="text-sm font-medium text-gray-500 mb-1">
-                  Choose the most accurate address
-                </label>
-                <select
-                  className="border-2 border-gray-300 px-1 rounded-md mb-2"
-                  form="form"
-                  name="province"
-                  onChange={addressSelected}
-                  ref={provinceSelectRef}
-                >
-                  <option value=""></option>
-                  {Object.entries(addresses).map((item, i) => (
-                    <option value={item[1]} key={i}>{item[0]}</option>
-                  ))}
-                </select>
-                <h1 className="text-lg font-semibold">Data</h1>
-                <div className="mt-1 flex items-center">
-                  <input
-                    type="file"
-                    id="upload"
-                    hidden
-                    onChange={getFile}
-                    form="form"
-                    name="geo_data_file"
-                  />
-                  <label
-                    htmlFor="upload"
-                    className="bg-gray-300 font-semibold text-sm py-1 px-3 rounded-l-md"
-                  >
-                    Upload
-                  </label>
-                  <Button
-                    title="Draw"
-                    className="bg-emerald-400 text-white font-semibold py-1 px-3 text-sm rounded-r-md"
-                    onClick={() => setShowToolbar(!showToolbar)}
-                  />
-                  {showToolbar && (
-                    <div className="flex ml-5 gap-2">
-                      <div
-                        className="w-7 h-7 bg-gray-300 rounded-md flex justify-center items-center"
-                        data-type="Polygon"
-                        onClick={draw}
-                      >
-                        <ShapePolygon />
-                      </div>
-                      <div
-                        className="w-7 h-7 bg-gray-300 rounded-md flex justify-center items-center"
-                        data-type="LineString"
-                        onClick={draw}
-                      >
-                        <OutlinePolyline />
-                      </div>
-                      <div
-                        className="w-7 h-7 bg-gray-300 rounded-md flex justify-center items-center"
-                        data-type="Point"
-                        onClick={draw}
-                      >
-                        <OutlineLocationMarker />
-                      </div>
-                      <div
-                        className="w-7 h-7 bg-gray-300 rounded-md flex justify-center items-center"
-                        onClick={() => map.removeInteraction(drawObj.current)}
-                      >
-                        <MousePointer
-                          onClick={() => map.removeInteraction(drawObj.current)}
-                        />
-                      </div>
-                    </div>
-                  )}
-                </div>
-                <div className="mt-3 flex gap-[2%]">
-                  <DragAndDrop setDragedFile={setDragedFile}>
-                    <Map
-                      center={fromLonLat(center)}
-                      zoom={zoom}
-                      className="w-[100%] h-[100%] "
-                    >
-                      <Layers>
-                        <TileLayer source={xyzSource} zIndex={0} />
-                        {geoFileObject && (
-                          <VectorLayer source={vectorSource.current} />
-                        )}
-                        <VectorLayer source={drawSource.current} />
-                      </Layers>
-                      <Contorols>
-                        <FullScreenControl />
-                      </Contorols>
-                    </Map>
-                  </DragAndDrop>
-                  <div className="w-[33%] max-h-72 overflow-y-scroll no-scrollbar">
-                    {Object.entries(labels).map((item, i) => (
-                      <DrawItem
-                        title={item[1]}
-                        dataId={item[0]}
-                        onClick={deleteItem}
-                        key={i}
-                      />
-                    ))}
-                  </div>
-                </div>
-              </div>
+              </form>
             </div>
           </div>
         </div>
--- src/components/pages/projectReport/index.jsx
+++ src/components/pages/projectReport/index.jsx
@@ -2,28 +2,39 @@ import React, { useEffect, useMemo, useRef, useState } from "react";
 import {
   Auth,
   Contorols,
+  DragAndDrop,
   FullScreenControl,
   Layers,
+  LeftModal,
   Map,
-  Modal,
+  ReportModal,
   TileLayer,
   VectorLayer,
 } from "../../shared";
-import { fromLonLat } from "ol/proj";
+import { fromLonLat, transform } from "ol/proj";
 import xyz from "../../../Source/xyz";
+import vector from "../../../Source/vector";
 import { useSearchParams } from "react-router-dom";
 import { OutlineArrowsExpand } from "../../icons";
 import { BASE_URL } from "../../../api";
 import VectorSource from "ol/source/Vector";
 import { loadFromUrl } from "../../../utils";
+import { getCenter } from "ol/extent";
 
 function ProjectReport() {
   const [params] = useSearchParams();
   const [center, setCenter] = useState([-100.9065, 57.9884]);
   const [zoom, setZoom] = useState(5);
-  const [showModal, setShowModal] = useState(true);
+  const [showReportModal, setShowReportModal] = useState(true);
+  const [showLeftModal, setShowLeftModal] = useState(true);
   const [detail, setDetail] = useState({});
+  const [showPending, setShowPending] = useState(true);
+  const [dragedFile, setDragedFile] = useState();
+  const [geoFileObject, setGeoFileObject] = useState();
+
   const vectorSourceRef = useRef(new VectorSource());
+  const drawSourceRef = useRef(vector({ wrapX: false }));
+  const drawObjRef = useRef();
 
   useEffect(() => {
     fetch(`${BASE_URL}/api/projects/${params.get("pid")}`, {
@@ -31,7 +42,10 @@ function ProjectReport() {
       credentials: 'include'
     })
       .then((res) => res.json())
-      .then((data) => setDetail(data));
+      .then((data) => {
+        setDetail(data);
+        setShowPending(data.state === 0);
+      });
   }, []);
 
   const xyzSource = useMemo(() => {
@@ -42,46 +56,76 @@ function ProjectReport() {
   }, []);
 
   useEffect(() => {
-    async function load() {
+    async function load(url) {
       try {
-        await loadFromUrl(detail.geo_data_file, vectorSourceRef.current, setCenter);
+        await loadFromUrl(url, vectorSourceRef.current);
+
+        const polygonCenter = getCenter(vectorSourceRef.current.getExtent());
+        
+        setCenter(transform(polygonCenter, "EPSG:3857", "EPSG:4326"));
         setZoom(4);
       } catch(err) {
         alert(err.message);
       }
     }
 
-    if (detail.geo_data_file) load();
+    switch(detail.state) {
+      case 0:
+        break
+      case 1:
+        load(detail.query_report_url);
+        break
+      case -1:
+        load(detail.geo_data_file);
+        break
+    }
   }, [detail]);
 
   return (
     <Auth>
-      {" "}
       <div className="relative">
-        <Map
-          center={fromLonLat(center)}
-          zoom={zoom}
-          className="w-[100vw] h-[100vh]"
-        >
-          <Layers>
-            <TileLayer source={xyzSource} zIndex={0} />
-            {detail.geo_data_file && (
-              <VectorLayer source={vectorSourceRef.current} />
-            )}
-          </Layers>
-          <Contorols>
-            <FullScreenControl />
-          </Contorols>
-        </Map>
-        {!showModal && (
+        <DragAndDrop setDragedFile={setDragedFile} className="w-[100%] h-[100vh]">
+          <Map
+            center={fromLonLat(center)}
+            zoom={zoom}
+            className="w-[100%] h-[100%]"
+          >
+            <Layers>
+              <TileLayer source={xyzSource} zIndex={0} />
+              {geoFileObject && (
+                <VectorLayer source={vectorSourceRef.current} />
+              )}
+              <VectorLayer source={drawSourceRef.current} />
+            </Layers>
+            <Contorols>
+              <FullScreenControl />
+            </Contorols>
+          </Map>
+        </DragAndDrop>
+        {!showReportModal && (
           <div
             className="absolute bottom-3 right-3 px-2 py-2 bg-emerald-400 rounded-lg"
-            onClick={() => setShowModal(true)}
+            onClick={() => setShowReportModal(true)}
           >
             <OutlineArrowsExpand size="1.5rem" color="white" />
           </div>
         )}
-        {showModal && <Modal setShowModal={setShowModal} />}
+        {showReportModal && <ReportModal setShowModal={setShowReportModal} />}
+
+        {showLeftModal && <LeftModal setShowModal={setShowLeftModal} pid={params.get("pid")} disabled={detail.state !== 0}/>}
+        
+        {showPending && <div style={{
+          position: 'absolute', 
+          top: 0,
+          bottom: 0, 
+          right: 0, 
+          left: 0,
+          margin: 'auto',
+          width: '40px', 
+          height: '40px', 
+          border: '1px solid red',
+          borderRadius: '50%'
+        }}></div>}
       </div>
     </Auth>
   );
